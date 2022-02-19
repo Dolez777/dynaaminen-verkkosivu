666bd90db674d032684ad0fdd0b2556f374c60bf
Dolez777 <73564898+Dolez777@users.noreply.github.com>
2022-02-19 10:25:00 +0200

    get dogs by breed completed

--- src/api/getDogs.js
+++ src/api/getDogs.js
@@ -1,10 +1,21 @@
 import axios from "../../node_modules/axios/index";
 
-const getDogs = async (amount) => {
-	return await axios
-		.get("https://dog.ceo/api/breeds/image/random/" + amount)
-		.then((response) => response.data.message)
-		.catch((err) => err);
+const getDogs = async (amount, breed) => {
+	if (breed === "random") {
+		return await axios.get(
+			"https://dog.ceo/api/breeds/image/random/" + amount
+		);
+	} else {
+		return await axios.get(
+			`https://dog.ceo/api/breed/${breed.replace(
+				"-",
+				"/"
+			)}/images/random/` + amount
+		);
+	}
 };
-
+/**https://dog.ceo/api/breed/
+African
+/images/random
+ */
 export default getDogs;
--- src/components/grid/index.jsx
+++ src/components/grid/index.jsx
@@ -2,49 +2,24 @@ import { useEffect, useState } from "react";
 import "./grid.scss";
 import getDogs from "./../../api/getDogs";
 
-function DogGrid() {
-	/**State johon laitetaan kuva arrayta mitä näytetään UI:ssa */
-	const [imageUrls, setImageUrls] = useState([""]);
-	/**Loading state */
-	const [loading, setLoading] = useState(false);
-	/**Functio jolla haetaan dog apista kuva urlit ja tallennetaan stateen*/
-	const handleGetDogs = () => {
-		setLoading(true);
-
-		/**Kutsutaan getDogs functiota arvolla 9 */
-		getDogs(9)
-			/**Kun axios request on valmis asetetaan response(kuvat) stateen */
-			.then((response) => setImageUrls(response))
-			.then(() => setLoading(false))
-			.catch((error) => {
-				setLoading(false);
-				/**Tässä voidaan rakentaa logiikkaa mahdollien virheiden käsittelylle */
-			});
-	};
-	/**useEffect on reactin vakio hook functio kuten myös kaikki use alkuiset functiot
-	 * Aina kun komponentti renderoidaan ensimmäisen kerran useEffect ajetaan
-	 * useEffectin tyhjään arrayhyn voi laittaa jonkun useStaten kun se state muuttuu useEffect ajetaan uudestaan
-	 */
-	useEffect(() => {
-		handleGetDogs();
-	}, []);
-	/**Loopataan array state array.map:illa
-	 * Kaikilla arraylla on functioita joita voidaan käyttää niiden muokkaamiseen esim .map
-	 * map functio käy jokaisen arrayn indexin läpi ja palauttaa käyttäjän määrittämän value/muuttujan uuteen arrayhyn
-	 * Lähes saman toiminnallisuuden voi tehdä forEach functiolla mutta forEach functio ei palauta uutta arrayta vaan muokkaa alkuperäistä
-	 */
-	const renderDog = imageUrls.map((url) => {
-		return <img className="dog-pic" src={url} alt=""></img>;
+function DogGrid(props) {
+	const renderDog = props.imageUrls.map((url, index) => {
+		return (
+			<div
+				key={index}
+				className="dog-pic"
+				style={{ backgroundImage: `url(${url})` }}
+			></div>
+		);
 	});
-	if (loading) {
+	if (props.loading) {
 		return <div>Loading...</div>;
+	} else if (props.error) {
+		return <div>ERORR DESTRUCKTION</div>;
 	} else {
 		/**HTML ja jsx elementtejen sisälle muuttujia voidaan käyttää laittamalla ne {} sulkeisiin */
 		return (
 			<div className="">
-				<button onClick={handleGetDogs} className="dog-button">
-					Random Dogs
-				</button>
 				<div className="image-grid">{renderDog}</div>
 			</div>
 		);
